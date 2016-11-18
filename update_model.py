9efe2dfca6d229969f5aec257ab8a6a1aea50c64
Rehan Ahmed <ahmeshaf.rehan@gmail.com>
2016-11-19 01:48:39 +0530

    adding requirements file and implemented updating function

--- update_model.py
+++ update_model.py
@@ -1,7 +1,80 @@
-'''
+"""
 TODO:
 
 define a function which takes in inputs word2vec model, and input text.
 Then update the model with extra vocabulary found in input text
 return updated model
-'''
+"""
+
+
+import gensim
+import logging
+
+from numpy import REAL, array, zeros, ones
+
+
+def update(model, data, num_sentences, mincount=3):
+    """
+    Add new words from new data to the existing model's vocabulary,
+    generate for them random vectors in syn0 matrix.
+    For existing words, increase their counts by their frequency in the new data.
+    Generate new negative sampling matrix (syn1neg).
+    Then, train the existing model with the new data.
+    """
+    added_count = 0
+
+    logging.info("Extracting vocabulary from new data...")
+    newmodel = gensim.models.Word2Vec(min_count=1, sample=0, hs=0)
+    newmodel.build_vocab(data)
+
+    logging.info("Merging vocabulary from new data...")
+    sampleint = model.vocab[model.index2word[0]].sample_int
+    words = 0
+    newvectors = []
+    newwords = []
+    for word in newmodel.vocab:
+        words += 1
+        if word not in model.vocab:
+            v = gensim.models.word2vec.Vocab()
+            v.index = len(model.vocab)
+            model.vocab[word] = v
+            model.vocab[word].count = newmodel.vocab[word].count
+            model.vocab[word].sample_int = sampleint
+            model.index2word.append(word)
+
+            random_vector = model.seeded_vector(model.index2word[v.index] + str(model.seed))
+            newvectors.append(random_vector)
+
+            added_count += 1
+            newwords.append(word)
+        else:
+            model.vocab[word].count += newmodel.vocab[word].count
+        if words % 1000 == 0:
+            logging.info("Words processed: %s" % words)
+
+    logging.info("added %d words into model from new data" % (added_count))
+
+    logging.info("Adding new vectors...")
+    alist = [row for row in model.syn0]
+    for el in newvectors:
+        alist.append(el)
+    model.syn0 = array(alist)
+
+    logging.info("Generating negative sampling matrix...")
+    model.syn1neg = zeros((len(model.vocab), model.layer1_size), dtype=REAL)
+    model.make_cum_table()
+
+    model.neg_labels = zeros(model.negative + 1)
+    model.neg_labels[0] = 1.
+
+    model.syn0_lockf = ones(len(model.vocab), dtype=REAL)
+
+    logging.info("Training with new data...")
+    model.train(data, total_examples=num_sentences)
+
+    return model
+
+
+def update_model(model, text_file, sentence_length=56):
+    sentences = gensim.models.word2vec.LineSentence(text_file, max_sentence_length=sentence_length)
+    return update(model, sentences, len(sentences))
